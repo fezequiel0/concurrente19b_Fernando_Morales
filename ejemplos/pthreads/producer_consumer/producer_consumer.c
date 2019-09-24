66603c3a4ecd1fc48c57ce623453c2728bded744
Fernando Morales <fezequielmorales@gmail.com>
2019-09-24 11:48:10 -0600

    Resolucion de producer_consumer

--- ejemplos/pthreads/producer_consumer/producer_consumer.c
+++ ejemplos/pthreads/producer_consumer/producer_consumer.c
@@ -3,128 +3,176 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
+#include <semaphore.h>
 
 typedef struct{
-	size_t numberOfThreads;
-	size_t position;  
-	pthread_mutex_t position_mutex;
+	size_t buffer_size;  
+	double* buffer;
+	size_t rounds;
+	useconds_t min_producer_delay; 
+	useconds_t max_producer_delay;
+	useconds_t min_consumer_delay;
+	useconds_t max_consumer_delay;
+	pthread_mutex_t stdout_mutex;
+	sem_t producer_semaphore;
+	sem_t consumer_semaphore;
 } shared_data_t;
 
-typedef struct{
-	size_t threadCounter; 
-	shared_data_t* shared_data;
-} private_data_t;
-
+int analyze_arguments(int argc, char* argv[],shared_data_t* shared_data);
 int create_threads(shared_data_t* shared_data);
-void* run(void* data);
-	
+void* produce(void* data);
+void* consume(void* data);
+void random_sleep(useconds_t min_milliseconds, useconds_t max_milliseconds);
+
 int main(int argc, char* argv[]){
-	
+	srand( time(NULL) );
 	shared_data_t* shared_data = calloc(1, sizeof(shared_data_t));
 	
+
 	if(shared_data == NULL){
 		return (void)fprintf(stderr, "ERROR: could not allocate shared memory\n") , 2;
 	}
-	
-	if(argc>1){
-		shared_data-> numberOfThreads = atoi(argv[1]);
-	}
-	else{
-		shared_data-> numberOfThreads = sysconf(_SC_NPROCESSORS_ONLN);
-	}
-	
-	pthread_mutex_init(&shared_data->position_mutex, /*attr*/ NULL);
-	shared_data-> position = 0;
-	
-	struct timespec start_time;
-	clock_gettime(CLOCK_MONOTONIC, &start_time);
-	
-	int error = create_threads(shared_data);
-	if(error){
-			return error;
-	}
-	
-	struct timespec finish_time;
-	clock_gettime(CLOCK_MONOTONIC, &finish_time);
 		
-	double elapsed_seconds = finish_time.tv_sec - start_time.tv_sec	+ 1e-9 * (finish_time.tv_nsec - start_time.tv_nsec);
-	
-	printf("Hello execution time %.9lf s\n", elapsed_seconds);
-	
-	pthread_mutex_destroy(&shared_data->position_mutex);
-	free(shared_data);
+	int error = analyze_arguments(argc,argv, shared_data);
+	if (error == 0){
+		
+		shared_data->buffer = (double*) calloc(shared_data->buffer_size, sizeof(double));
+		if(shared_data->buffer){
 			
-	return 0;
+			sem_init(&shared_data->producer_semaphore, 0 /*pshared*/ , shared_data->buffer_size);
+			sem_init(&shared_data->consumer_semaphore, 0 /*pshared*/ , 0);
+			pthread_mutex_init(&shared_data->stdout_mutex, /*attr*/ NULL);
+
+			struct timespec start_time;
+			clock_gettime(CLOCK_MONOTONIC, &start_time);
+	
+			error = create_threads(shared_data);
+			if(error == 0){
+				struct timespec finish_time;
+				clock_gettime(CLOCK_MONOTONIC, &finish_time);
+		
+				double elapsed_seconds = finish_time.tv_sec - start_time.tv_sec	+ 1e-9 * (finish_time.tv_nsec - start_time.tv_nsec);
+	
+				printf("Simulation time %.9lf s\n", elapsed_seconds);
+	
+			}
+		
+			pthread_mutex_destroy(&shared_data->stdout_mutex);
+			free(shared_data->buffer);
+		}
+		
+		else{
+			fprintf(stderr, "error: could not allocate memory for %zu products\n", shared_data->buffer_size);
+			error = 2;
+		}
+	
+	}
+	
+	free(shared_data);
+	return error;
+}
+
+int analyze_arguments(int argc, char* argv[],shared_data_t* shared_data){
+	
+	if( argc != 7){
+		fprintf(stderr, "usage: producer_consumer buffer_size rounds min_producer_delay max_producer_delay min_consumer_delay max_consumer_delay ");
+		return 1;
+	}
+	
+	shared_data->buffer_size = strtoull(argv[1],NULL,10);
+	if(shared_data->buffer_size == 0){
+		return 2;
+	}
+	
+	if(sscanf(argv[2], 	"%zu", &shared_data->rounds) != 1 || shared_data->rounds == 0){
+		return (void)fprintf(stderr,"invalid rounds: %s\n", argv[2]), 2;	
+	}
+	
+	if(sscanf(argv[3], 	"%u", &shared_data->min_producer_delay) != 1){
+		return (void)fprintf(stderr,"invalid min producer delay: %s\n", argv[3]), 3;	
+	}
+	
+	if(sscanf(argv[4], 	"%u", &shared_data->max_producer_delay) != 1 || shared_data->max_producer_delay < shared_data->min_producer_delay){
+		return (void)fprintf(stderr,"invalid max producer delay: %s\n", argv[4]), 4;	
+	}
+	
+	if(sscanf(argv[5], 	"%u", &shared_data->min_consumer_delay) != 1){
+		return (void)fprintf(stderr,"invalid min consumer delay: %s\n", argv[5]), 5;	
+	}
+
+	if(sscanf(argv[6], 	"%u", &shared_data->max_consumer_delay) != 1 || shared_data->max_consumer_delay < shared_data->min_consumer_delay){
+		return (void)fprintf(stderr,"invalid max consumer delay: %s\n", argv[6]), 6;	
+	}
+	
+	return EXIT_SUCCESS;
 }
 
 int create_threads(shared_data_t* shared_data){
-	pthread_t* thread_v = malloc(shared_data->numberOfThreads * sizeof(pthread_t));
 	
-	if(thread_v == NULL){
-		return (void)fprintf(stderr, "ERROR: could not allocate memory for %zu threads\n", shared_data-> numberOfThreads) , 1; 
-	}
-	
-	private_data_t* private_data = calloc(shared_data-> numberOfThreads, sizeof(private_data_t));
-	
-	if(private_data == NULL){
-		return (void)fprintf(stderr, "ERROR: could not allocate private memory for %zu threads\n", shared_data->numberOfThreads) , 3;
-	}
+	pthread_t producer_thread;
+	pthread_t consumer_thread;
 				
-	for(size_t index=0; index<shared_data->numberOfThreads ;++index){																																																																													
-		private_data[index].threadCounter = index;
-		private_data[index].shared_data = shared_data;
-		pthread_create(&thread_v[index], NULL, run, &private_data[index]);
-	}
+	pthread_create(&producer_thread, NULL, produce, shared_data);
+	pthread_create(&consumer_thread, NULL, consume, shared_data);
 	
-	pthread_mutex_lock(&shared_data->position_mutex);
-	
-	printf("Hello world from main thread\n");
-	
-	pthread_mutex_unlock(&shared_data->position_mutex);
- 	
-	for(size_t index=0; index<shared_data->numberOfThreads ;++index){
-		pthread_join(thread_v[index], NULL);
-	}	
-		
-	free(private_data);
-	free(thread_v);
+	pthread_join(producer_thread, NULL);
+	pthread_join(consumer_thread, NULL);
 	
 	return 0;
 }
 
-void* run(void* data){
-	private_data_t * private_data = (private_data_t*)data;
- 	shared_data_t* shared_data = private_data->shared_data;
+void* produce(void* data){
+	shared_data_t* shared_data = (shared_data_t*)data;
  	
- 	size_t threadCounter = private_data->threadCounter;
- 	size_t numberOfThreads = shared_data->numberOfThreads; 
+ 	for(size_t round =1; round <=shared_data->rounds; ++round){
+		for(size_t index = 0; index < shared_data->buffer_size; ++index){
+			
+			sem_wait(&shared_data->producer_semaphore);
+			shared_data->buffer[index] = round + (index+1.0) / 100.0;//CODIGO QUEMADO ALERT: simplemente se usa 100 arbitrariamente
+				
+			random_sleep(shared_data->min_producer_delay, shared_data->max_producer_delay);
+		
+			pthread_mutex_lock(&shared_data->stdout_mutex);
+			printf("Produced %.2lf\n", shared_data->buffer[index]);
+			pthread_mutex_unlock(&shared_data->stdout_mutex);
  	
- 	pthread_mutex_lock(&shared_data->position_mutex);
- 	
- 	++shared_data->position;
-	printf("Thread %zu / %zu : I arrived at position %zu\n", threadCounter, numberOfThreads, shared_data->position);
- 	
- 	pthread_mutex_unlock(&shared_data->position_mutex);
+			sem_post(&shared_data->consumer_semaphore);
+		}
+	} 	
  	
 	return NULL;
 }
 
+void* consume(void* data){
+	shared_data_t* shared_data = (shared_data_t*)data;
+
+	for(size_t round =1; round <= shared_data->rounds; ++round){
+		for(size_t index = 0; index < shared_data->buffer_size; ++index){
+		
+			sem_wait(&shared_data->consumer_semaphore);
+		
+			random_sleep(shared_data->min_consumer_delay, shared_data->max_consumer_delay);
+		
+			pthread_mutex_lock(&shared_data->stdout_mutex);
+ 			printf("\t\t\tConsumed %.2lf\n", shared_data->buffer[index]);
+ 			pthread_mutex_unlock(&shared_data->stdout_mutex);
+ 	
+			
+			sem_post(&shared_data->producer_semaphore);
+		}
+	} 	
+	return NULL;
+}
+
+void random_sleep(useconds_t min_milliseconds, useconds_t max_milliseconds){
+	useconds_t duration = min_milliseconds;
+	useconds_t range = max_milliseconds - min_milliseconds;
+	if( range > 0){
+		duration += rand() % range;
+	}	
+	usleep(1000 * duration);
+}
+
 /* 
- Condicion de carrera: cuando dos procesos intentan modificar el mismo archivo al mismo tiempo
- Deben ocurrir tres cosas:
- 1. Codigo concurrente
- 2. Memoria compartida
- 3. Modificacion y escritura, nunca lectura;
- Codigo que genera condiciones de carrera: region critica
- valgrind --tool=helgrind ./position_race
- La memoria en el heap puede ser compartida o privada, a la privada  solo vamos a poder accesarlas con punteros
-
-
- Mecanismos de control de flujo -> while, if, for....
- Mecanismos de control de concurrencia = mecanismos de sincronizacion  
-	Mutex(lock): mut-ual -ex-clusion -> cuello de botella, unicamente deja pasar a un proceso -> se interpreta como un valor booleano
-	Semaphore: 
-	Barrier:
-	Condition variable:
-	RW locks:
+ Separacion de tareas / separacacion de asuntos / paralelismo de tareas -> mismos datos, diferentes tareas
 */
