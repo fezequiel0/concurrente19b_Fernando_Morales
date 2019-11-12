481a1e1170ab175487d19f7a03b32ac8094a2064
Fernando Morales <fezequielmorales@gmail.com>
2019-11-12 11:51:26 -0600

    Hello en MPI

---
+++ ejemplos/mpi/hello/hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char* argv[]){
+	MPI_Init(&argc, &argv); 
+
+	int my_rank = -1;
+	int process_count = -1;
+	
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);//siempre se usa ese mecanismo de comunicacion
+	MPI_Comm_size(MPI_COMM_WORLD, &process_count);
+
+	char hostname[MPI_MAX_PROCESSOR_NAME]; 
+	int hostname_length = -1;
+	MPI_Get_processor_name(hostname, &hostname_length);
+	std::cout << "Hello from main thread of process " << my_rank << " of " << process_count  << " on " << hostname << "\n";
+
+	MPI_Finalize();
+	return 0;
+}
+
+// parametro -n x es para correr una cantidad x de procesos en la maquina presente
