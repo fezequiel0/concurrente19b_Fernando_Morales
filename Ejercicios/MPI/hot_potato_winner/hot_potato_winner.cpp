ef642546d07b98a0160130d47a40f33c472b8315
Fernando Morales <fezequielmorales@gmail.com>
2019-12-14 00:43:57 -0600

    Resolución de hot_potato_winner

--- Ejercicios/MPI/hot_potato_winner/hot_potato_winner.cpp
+++ Ejercicios/MPI/hot_potato_winner/hot_potato_winner.cpp
@@ -20,7 +20,8 @@ int main(int argc, char* argv[])
     int original_hot_potato = 0;
     int hot_potato = 0;
     int number_processes_out = 0;
-	if ( argc >= 2 ){
+    int im_out = 0;
+    	if ( argc >= 2 ){
 		original_hot_potato = atoi(argv[1]);
 	}
 	else
@@ -42,9 +43,11 @@ int main(int argc, char* argv[])
         hot_potato = original_hot_potato; 
         --hot_potato;
         if(hot_potato > 0){
+            MPI_Send(&number_processes_out, 1, MPI_INT, my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
             MPI_Send(&hot_potato, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
         }
         else{
+            im_out = 1;
             ++number_processes_out;
             MPI_Send(&number_processes_out, 1, MPI_INT, my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
             MPI_Send(&original_hot_potato, 1, MPI_INT, my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
@@ -53,9 +56,7 @@ int main(int argc, char* argv[])
     while(hot_potato > 0){
         MPI_Recv(&number_processes_out, /*capacity*/ 1, MPI_INT, /*source*/ MPI_ANY_SOURCE, /*tag*/ 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Recv(&hot_potato, /*capacity*/ 1, MPI_INT, /*source*/ MPI_ANY_SOURCE, /*tag*/ 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        --hot_potato;
-        if(hot_potato == 0 ){
-            ++number_processes_out;
+        if(im_out && number_processes_out != process_count - 1){
             if ( my_rank < process_count - 1 ){
                 MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
                 MPI_Send(&original_hot_potato, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
@@ -65,34 +66,41 @@ int main(int argc, char* argv[])
                 MPI_Send(&original_hot_potato, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
             }
         }
-	    else{
-            if(hot_potato > 0){
-                if(number_processes_out == process_count - 1){
-                    std::cout << "The process with rank "<< my_rank << " won."<< std::endl;
-                    hot_potato = -1;    
-                    for ( int destination = 0; destination < process_count; ++destination ){
-                        if(destination != my_rank){
-				            MPI_Send(&number_processes_out, 1, MPI_INT, destination, /*tag*/ 0, MPI_COMM_WORLD);
-				            MPI_Send(&hot_potato, 1, MPI_INT, destination, /*tag*/ 0, MPI_COMM_WORLD);
-                        }
-	        		}
-                    // if ( my_rank < process_count - 1 ){
-                    //     MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-                    //     MPI_Send(&hot_potato, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-                    // }
-                    // else{
-                    //     MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
-                    //     MPI_Send(&hot_potato, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
-                    // }
+        else{
+            --hot_potato;
+            if(hot_potato == 0 ){
+                ++number_processes_out;
+                im_out = 1;
+                if ( my_rank < process_count - 1 ){
+                    MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
+                    MPI_Send(&original_hot_potato, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
                 }
                 else{
-                    if ( my_rank < process_count - 1 ){
-                        MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-                        MPI_Send(&hot_potato, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
+                    MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
+                    MPI_Send(&original_hot_potato, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
+                }
+            }
+            else{
+                if(hot_potato > 0){
+                    if(number_processes_out == process_count - 1){
+                        std::cout << "The process with rank "<< my_rank << " won."<< std::endl;
+                        hot_potato = -1;    
+                        for ( int destination = 0; destination < process_count; ++destination ){
+                            if(destination != my_rank){
+                                MPI_Send(&number_processes_out, 1, MPI_INT, destination, /*tag*/ 0, MPI_COMM_WORLD);
+                                MPI_Send(&hot_potato, 1, MPI_INT, destination, /*tag*/ 0, MPI_COMM_WORLD);
+                            }
+                        }
                     }
                     else{
-                        MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
-                        MPI_Send(&hot_potato, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
+                        if ( my_rank < process_count - 1 ){
+                            MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
+                            MPI_Send(&hot_potato, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
+                        }
+                        else{
+                            MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
+                            MPI_Send(&hot_potato, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
+                        }
                     }
                 }
             }
