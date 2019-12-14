9ad75d5e618b5c0da7a198ba2798361fb352b237
Fernando Morales <fezequielmorales@gmail.com>
2019-12-14 10:33:21 -0600

    Resolución de hot_potato_winner

--- Ejercicios/MPI/hot_potato_winner/hot_potato_winner.cpp
+++ Ejercicios/MPI/hot_potato_winner/hot_potato_winner.cpp
@@ -1,111 +1,76 @@
-#include <mpi.h>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <cstdlib>
 #include <iostream>
+#include <mpi.h>
+#include <omp.h>
 
-#define MESSAGE_CAPACITY 1024
+#define MAX_HOSTNAME_LENGTH 1024
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	MPI_Init(&argc, &argv);
 
-	int my_rank = -1;
-	int process_count = -1;
+	int my_rank = -1; 
+	int total_processes = -1;
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &process_count);
+	MPI_Comm_size(MPI_COMM_WORLD, &total_processes);
 
-	char hostname[MPI_MAX_PROCESSOR_NAME];
+	char hostname[MAX_HOSTNAME_LENGTH];
 	int hostname_length = -1;
 	MPI_Get_processor_name(hostname, &hostname_length);
 
-    int original_hot_potato = 0;
-    int hot_potato = 0;
-    int number_processes_out = 0;
-    int im_out = 0;
-    	if ( argc >= 2 ){
-		original_hot_potato = atoi(argv[1]);
+	int hot_potato = 0;
+	if(!sscanf(argv[1],"%d", &hot_potato)) {
+		if(my_rank == 0) {
+			MPI_Finalize();
+			return printf("Error in arguments.\nUse ./hot_patato_loser patatoValue\n"), 1;
+		}
 	}
-	else
-	{
-		if ( my_rank == 0 )
-		{
-			std::cin >> hot_potato;
-			for ( int destination = 1; destination < process_count; ++destination )
-			{
-				MPI_Send(&original_hot_potato, 1, MPI_INT, destination, /*tag*/ 0, MPI_COMM_WORLD);
+
+	if(total_processes < 2) {
+		MPI_Finalize();
+		return printf("You can't play alone.\n"), 2;
+	}
+
+	int current = 0;
+	int processes_still_playing = total_processes - 1;
+	bool continuePlaying = true;
+	
+	while(processes_still_playing >= 1) {
+		
+		if(current != 0 || my_rank != 0) {
+			int prev = my_rank -  1;
+			if (prev < 0) {
+				prev = total_processes - 1;
+			}
+			MPI_Recv(&hot_potato, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&processes_still_playing, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}	
+		++current;
+		if(continuePlaying) {
+			--hot_potato;
+			if(hot_potato == 0) {
+				--processes_still_playing;
+				continuePlaying = false;
+				int place = total_processes - processes_still_playing - 1;
+				printf("Process %d lost in place %d.\n", my_rank, place);
+				sscanf(argv[1], "%d", &hot_potato);
 			}
 		}
-		else{
-			MPI_Recv(&original_hot_potato, 1, MPI_INT, /*source*/ 0, /*tag*/ 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		if(players >= 0) {
+			int next = (my_rank + 1) % total_processes;
+			MPI_Send(&hot_potato, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
+			MPI_Send(&processes_still_playing, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
 		}
+			
 	}
 
-    if(my_rank == 0){
-        hot_potato = original_hot_potato; 
-        --hot_potato;
-        if(hot_potato > 0){
-            MPI_Send(&number_processes_out, 1, MPI_INT, my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-            MPI_Send(&hot_potato, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-        }
-        else{
-            im_out = 1;
-            ++number_processes_out;
-            MPI_Send(&number_processes_out, 1, MPI_INT, my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-            MPI_Send(&original_hot_potato, 1, MPI_INT, my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-        }
-    }
-    while(hot_potato > 0){
-        MPI_Recv(&number_processes_out, /*capacity*/ 1, MPI_INT, /*source*/ MPI_ANY_SOURCE, /*tag*/ 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&hot_potato, /*capacity*/ 1, MPI_INT, /*source*/ MPI_ANY_SOURCE, /*tag*/ 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        if(im_out && number_processes_out != process_count - 1){
-            if ( my_rank < process_count - 1 ){
-                MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-                MPI_Send(&original_hot_potato, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-            }
-            else{
-                MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
-                MPI_Send(&original_hot_potato, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
-            }
-        }
-        else{
-            --hot_potato;
-            if(hot_potato == 0 && !im_out){
-                ++number_processes_out;
-                im_out = 1;
-                if ( my_rank < process_count - 1 ){
-                    MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-                    MPI_Send(&original_hot_potato, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-                }
-                else{
-                    MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
-                    MPI_Send(&original_hot_potato, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
-                }
-            }
-            else{
-                if(hot_potato > 0){
-                    if(number_processes_out == process_count - 1){
-                        std::cout << "The process with rank "<< my_rank << " won."<< std::endl;
-                        hot_potato = -1;    
-                        for ( int destination = 0; destination < process_count; ++destination ){
-                            if(destination != my_rank){
-                                MPI_Send(&number_processes_out, 1, MPI_INT, destination, /*tag*/ 0, MPI_COMM_WORLD);
-                                MPI_Send(&hot_potato, 1, MPI_INT, destination, /*tag*/ 0, MPI_COMM_WORLD);
-                            }
-                        }
-                    }
-                    else{
-                        if ( my_rank < process_count - 1 ){
-                            MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-                            MPI_Send(&hot_potato, /*count*/ 1, MPI_INT, /*dest*/ my_rank + 1, /*tag*/ 0, MPI_COMM_WORLD);
-                        }
-                        else{
-                            MPI_Send(&number_processes_out, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
-                            MPI_Send(&hot_potato, /*count*/ 1, MPI_INT, /*dest*/ 0, /*tag*/ 0, MPI_COMM_WORLD);
-                        }
-                    }
-                }
-            }
-        }
-    }
+	if(continuePlaying) {
+		printf("Process %d is the winner.\n", my_rank);
+	}
 
 	MPI_Finalize();
+
+	return 0;
 }
